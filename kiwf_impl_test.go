1760d9bec382a14186629e4ffee22f7bfa06ebfa
Patrick de Kievit <patrick@tisgroup.nl>
2022-12-19 17:09:27 +0100

    [] tests

--- kiwf_impl_test.go
+++ kiwf_impl_test.go
@@ -5,16 +5,15 @@ import (
 	"time"
 )
 
-var exitCalled = false
-var testTitle = ""
-
 func Test_kiwf_Impl_1(t *testing.T) {
-	timeout := 1 * time.Second
-	exitCalled = false
 
-	exitFunc := func(_ string, _ map[string]interface{}) {
-		exitCalled = true
-	}
+	var (
+		timeout    = 1 * time.Second
+		exitCalled = false
+		exitFunc   = func(_ string, _ map[string]interface{}) {
+			exitCalled = true
+		}
+	)
 
 	kiwf, err := New("test case 1", &Config{
 		DelayStartupTime: 50 * time.Millisecond,
@@ -22,12 +21,12 @@ func Test_kiwf_Impl_1(t *testing.T) {
 		ExitFunction:     &exitFunc,
 	})
 	if err != nil {
-		t.Errorf("got error '%s'", err.Error())
+		t.FailNow()
 	}
 
 	kiwf.Start()
+	defer kiwf.Close()
 	time.Sleep(200 * time.Millisecond)
-	kiwf.Close()
 
 	if time.Since(kiwf.LastAction()) > timeout {
 		t.Errorf("should be longer than timeout,last touched %v, got timeout %v", time.Since(kiwf.LastAction()), timeout)
@@ -40,8 +39,8 @@ func Test_kiwf_Impl_1(t *testing.T) {
 }
 
 func Test_kiwf_Impl_2(t *testing.T) {
-	exitCalled = false
-	testTitle = ""
+	var exitCalled = false
+	var testTitle = ""
 
 	var (
 		endTime = time.Now()
@@ -54,18 +53,20 @@ func Test_kiwf_Impl_2(t *testing.T) {
 		exitCalled = true
 	}
 
-	tt := "test case 2"
+	tt := "t2"
 	kiwf, err := New(tt, &Config{
 		DelayStartupTime: 1 * time.Second,
 		Timeout:          timeout,
 		ExitFunction:     &exitFunc,
 	})
 	if err != nil {
-		t.Errorf("got error '%s'", err.Error())
+		t.FailNow()
 	}
+
 	kiwf.Start()
+	defer kiwf.Close()
+
 	time.Sleep(2200 * time.Millisecond)
-	kiwf.Close()
 
 	// this should timeout
 	if time.Since(kiwf.LastAction()) < timeout {
@@ -84,6 +85,7 @@ func Test_kiwf_Impl_2(t *testing.T) {
 
 // Test_kiwf_Impl_3, we mimic a process that has a 5ms timeout running for 50ms and doing work
 func Test_kiwf_Impl_3(t *testing.T) {
+
 	kiwf, err := New("tc3", &Config{
 		DelayStartupTime: time.Nanosecond,
 		Timeout:          5 * time.Millisecond,
@@ -103,8 +105,7 @@ func Test_kiwf_Impl_3(t *testing.T) {
 }
 
 func Test_kiwf_Impl_4(t *testing.T) {
-	exitCalled = false
-	testTitle = ""
+
 	var (
 		testTimeout = 500 * time.Millisecond
 	)
@@ -112,31 +113,30 @@ func Test_kiwf_Impl_4(t *testing.T) {
 	if err == nil {
 		t.Error("should throw error empty title")
 	}
-
-	kiwf, _ := New("test case nil config", nil)
+	kiwf, err := New("test case nil config", nil)
+	if err != nil {
+		t.FailNow()
+	}
 
 	kiwf.Start()
+	defer kiwf.Close()
 	startTime := time.Now()
-	for {
+	for time.Since(startTime) < testTimeout {
 		kiwf.Tick()
-
-		if time.Since(startTime) > testTimeout {
-			break
-		}
 		time.Sleep(1 * time.Millisecond)
 	}
-	kiwf.Close()
+
 }
 
 func Test_kiwf_Impl_5(t *testing.T) {
 	// passtru test
-	exitCalled = false
-	testTitle = ""
-	var testVar = ""
-	var testPassed = "test123"
 	var (
-		endTime = time.Now()
-		timeout = 1 * time.Second
+		exitCalled = false
+		testTitle  = ""
+		testVar    = ""
+		testPassed = "test123"
+		endTime    = time.Now()
+		timeout    = 1 * time.Second
 	)
 
 	exitFunc := func(title string, passtru map[string]interface{}) {
@@ -155,8 +155,8 @@ func Test_kiwf_Impl_5(t *testing.T) {
 	})
 
 	kiwf.Start()
+	defer kiwf.Close()
 	time.Sleep(2200 * time.Millisecond)
-	kiwf.Close()
 
 	// this should timeout
 	if time.Since(kiwf.LastAction()) < timeout {
@@ -180,22 +180,14 @@ func Test_kiwf_Impl_5(t *testing.T) {
 // example on md file
 func Test_kiwf_Impl_6(t *testing.T) {
 
-	// internal settings
-	var (
-		timeout          = 150 * time.Millisecond
-		delayStartupTime = 10 * time.Millisecond
-	)
-
 	// simulation settings
 	var (
-		testTimeout     = 500 * time.Millisecond
-		workerSleepTime = 1 * time.Millisecond
+		testTimeout = 500 * time.Millisecond
 	)
 
-	tt := "my work"
-	kiwf, err := New(tt, &Config{
-		DelayStartupTime: delayStartupTime,
-		Timeout:          timeout,
+	kiwf, err := New("my work", &Config{
+		DelayStartupTime: 10 * time.Millisecond,
+		Timeout:          150 * time.Millisecond,
 	})
 	if err != nil {
 		t.Errorf("got error '%s'", err.Error())
@@ -204,12 +196,10 @@ func Test_kiwf_Impl_6(t *testing.T) {
 	kiwf.Start()
 	defer kiwf.Close()
 
-	for {
+	// simulate work and tick in iteration
+	for time.Since(startTime) < testTimeout {
 		kiwf.Tick()
-		if time.Since(startTime) > testTimeout {
-			break
-		}
-		time.Sleep(workerSleepTime)
+		time.Sleep(1 * time.Millisecond)
 	}
 	time.Sleep(time.Second)
 	// yep this panics
